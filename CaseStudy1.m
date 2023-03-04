3b10556b8e34170886ec323bd5663307da2b35e4
bmarymary <123192136+bmarymary@users.noreply.github.com>
2023-03-04 11:29:29 -0600

    Add files via upload

--- CaseStudy1.m
+++ CaseStudy1.m
@@ -1,12 +1,3 @@
-%% Case Study 1
-% Authors: Xander Schmit, Nicole Ejedimu, Mary Butler
-% Collaborators:
-% Date: 
-% Work Log
-% Who               When
-% Xander & Nicole   02/28 10:45 am - 12:15 pm
-% Xander, Mary, Nicole 02/03 2:30 - 5:00 pm
-%% Constants
 
 % Sampling Frequency
 fs = 44100; % kHz
@@ -88,3 +79,52 @@ treble_test_2 = lsim(b,a,sounddata(2,:)',t);
 treble_test = cat(2,treble_test_1, treble_test_2);
 
 sound(treble_test, Fs);
+
+%% Bass Range
+
+% LPF Calculations
+bass_lpf = 250;                     %bass lowpass frequency
+
+bass_C_lpf = 1/(2.*pi.*bass_lpf.*R_lpf);
+
+bass_tau_lpf = R_lpf.*bass_C_lpf; 
+
+bass_b_lpf = 1/bass_tau_lpf;
+
+bass_a_lpf = [1 1/bass_tau_lpf];
+
+
+% HPF Calculations
+bass_hpf = 20;                      %bass highpass frequency
+
+bass_C_hpf = 1/(2.*pi.*bass_hpf.*R_hpf); 
+
+bass_tau_hpf = R_hpf .* bass_C_hpf; 
+
+bass_b_hpf = [1 0];
+
+bass_a_hpf = [1 1/bass_tau_hpf];
+
+% System
+bass_a = [1 1./bass_tau_hpf + 1./bass_tau_lpf (1/bass_tau_hpf).*(1/bass_tau_lpf)];
+
+bass_b = [0 1/bass_tau_lpf 0];
+
+freqs(bass_b,bass_a);
+
+%%
+% Bass Test 
+bass_test_1 = lsim(bass_b,bass_a,sounddata(1,:),t);
+
+bass_test_2 = lsim(bass_b,bass_a,sounddata(2,:),t);
+
+bass_test = cat(2,bass_test_1, bass_test_2);
+
+sound(bass_test, Fs);
+
+
+
+
+
+
+
